e7164a417d841f994311f3b85b806578870568c0
José Ignacio Jeremías Aranda Baeza <jo.arandab@duocuc.cl>
2026-04-09 00:22:46 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -25,12 +25,12 @@ resource "aws_security_group" "ssh_access" {
 }
 
 resource "aws_instance" "mi_ec2" {
-  ami                    = "ami-0fa8aad99729521be"
-  instance_type          = "t2.micro" # Requerimiento 4 de tu pauta
+  ami                    = "ami-0ec10929233384c7f"
+  instance_type          = "t2.micro" 
   subnet_id              = aws_subnet.subnet_publica_1.id
   vpc_security_group_ids = [aws_security_group.ssh_access.id]
   
-  # Evita que se borre accidentalmente (Buena practica)
+  
   disable_api_termination = true
 
   root_block_device {
