31e880689e7a5e0345e9b6e1bf308f2097c82a22
José Ignacio Jeremías Aranda Baeza <jo.arandab@duocuc.cl>
2026-04-09 00:30:49 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "mi_vpc" {
-  cidr_block           = "10.1.0.0/16 "
+  cidr_block           = "10.1.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
